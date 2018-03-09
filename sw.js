74ceb235af8b594983fce92edc6c75d746fa10b2
baopeipei <baopeipei@smzdm.com>
2018-03-09 11:01:10 +0800

    ~

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('fetch', function(event) {
   if (/\.jpg$/.test(event.request.url)) {
-    event.respondWith(fetch('/logo.png'));
+    event.respondWith(fetch('logo.png'));
   }
 });
