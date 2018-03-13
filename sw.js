bb53445a4155d5632d4d27997a8c70aa7331bbaa
baopeipei <baopeipei@smzdm.com>
2018-03-13 11:52:46 +0800

    ~

--- sw.js
+++ sw.js
@@ -1,5 +1,28 @@
-self.addEventListener('fetch', function(event) {
-  if (/\.jpg$/.test(event.request.url)) {
-    event.respondWith(fetch('logo.png'));
-  }
+var cacheName = 'helloWorld';               
+self.addEventListener('install', event => { 
+  event.waitUntil(
+      caches.open(cacheName)                  
+  .then(cache => cache.addAll([
+      './logo.png',
+      './sqb_bg.jpg'
+  ]))
+  );
 });
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+      caches.match(event.request)
+    .then(function (response) {
+        if (response) {
+        return response;
+        }
+        return fetch(event.request);
+    })
+    );
+});
+
+// self.addEventListener('fetch', function(event) {
+//   if (/\.jpg$/.test(event.request.url)) {
+//     event.respondWith(fetch('logo.png'));
+//   }
+// });
